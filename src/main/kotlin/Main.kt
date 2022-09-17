731c92561eb925f980ff560b2e66c1b3e4b5d6dd
LucasAlfare <god-lucas@hotmail.com>
2022-09-17 01:09:41 -0300

    Updates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 @Suppress("MemberVisibilityCanBePrivate")
 class BytesReader(private val data: IntArray) {
 
@@ -6,13 +8,13 @@ class BytesReader(private val data: IntArray) {
   private val tmpBuffer = IntArray(4) { 0 }
 
   fun read1Byte(customPosition: Int = position): Int {
-    seek(customPosition, 1)
+    seek(customPosition)
     if (updateTmpBuffer(1)) return (tmpBuffer[0])
     return -1
   }
 
   fun read2Bytes(customPosition: Int = position): Int {
-    seek(customPosition, 2)
+    seek(customPosition)
     if (updateTmpBuffer(2)) {
       return tmpBuffer[0] shl 8 or
               tmpBuffer[1]
@@ -21,7 +23,7 @@ class BytesReader(private val data: IntArray) {
   }
 
   fun read3Bytes(customPosition: Int = position): Int {
-    seek(customPosition, 3)
+    seek(customPosition)
     if (updateTmpBuffer(3)) {
       return tmpBuffer[0] shl 16 or
               (tmpBuffer[1] shl 8 or
@@ -31,7 +33,7 @@ class BytesReader(private val data: IntArray) {
   }
 
   fun read4Bytes(customPosition: Int = position): Int {
-    seek(customPosition, 4)
+    seek(customPosition)
     if (updateTmpBuffer(4)) {
       return tmpBuffer[0] shl 24 or
               (tmpBuffer[1] shl 16 or
@@ -41,12 +43,12 @@ class BytesReader(private val data: IntArray) {
     return -1
   }
 
-  fun seek(pos: Int, nBytes: Int): Int {
-    if (data.size <= pos + nBytes) {
+  fun seek(nextPos: Int): Int {
+    if (nextPos < data.size) {
       return -1
     }
 
-    position = pos
+    position = nextPos
     return position
   }
 
@@ -58,40 +60,14 @@ class BytesReader(private val data: IntArray) {
   }
 }
 
-@Suppress("MemberVisibilityCanBePrivate")
-class BytesWriter(private val nBytes: Int) {
-
-  var position = 0
-  val data = IntArray(nBytes) { 0 }
-
-  fun writeByte(byte: Int, customPosition: Int = position) {
-    if (seek(customPosition) == -1) return
-    if (position + 1 >= nBytes) return
-    data[position++] = byte
-  }
-
-  fun writeInt16(int16: Int, customPosition: Int = position) {
-    writeByte((int16 shr 8) and 0xFF, customPosition + 0)
-    writeByte((int16 shr 0) and 0xFF, customPosition + 1)
-  }
-
-  fun writeInt24(int24: Int, customPosition: Int = position) {
-    writeByte((int24 shr 16) and 0xFF, customPosition + 0)
-    writeByte((int24 shr 8) and 0xFF, customPosition + 1)
-    writeByte((int24 shr 0) and 0xFF, customPosition + 2)
-  }
-
-  fun seek(pos: Int): Int {
-    if (pos >= nBytes) {
-      return -1
-    }
-    position = pos
-    return position
-  }
-}
-
 fun main() {
-  val writer = BytesWriter(4)
-  writer.writeInt24(0xaabbcc)
-  println(writer.data.map { Integer.toHexString(it) })
+  val f = File("src/main/resources/test_file.mid")
+  val bytes = f.readBytes()
+  val reader = BytesReader(bytes.map { it.toInt() }.toIntArray())
+  val MThd =
+          "${reader.read1Byte().toChar()}" +
+          "${reader.read1Byte().toChar()}" +
+          "${reader.read1Byte().toChar()}" +
+          "${reader.read1Byte().toChar()}"
+  println(MThd)
 }
